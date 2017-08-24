7ddd55bc6dce782e27735fdeaaffcf79f36e19de
Kevin Harrison <kharrison7@elon.edu>
2017-08-23 20:28:18 -0400

    This adds in the word generator and several middleware packages.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 let bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const adminRouter = require('./public/routes/admin');
 const gameRouter = require('./public/routes/gameplay');
 // const data = require('./items.js');
@@ -22,6 +24,9 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
+// This consolelogs a buch of actions
+app.use(logger('dev'));
+app.use(cookieParser());
 // Sets the view engine and router.
 app.engine('mustache', mustacheExpress());
 // use views folder to pick up views.
@@ -35,10 +40,26 @@ app.use('/gameplay', gameRouter);
 app.use(express.static(__dirname + '/public'));
 
 
+// This sets up the session.
+app.use(session({
+  secret: 'variant',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // This begins the interesting code:
 // This begins the interesting code:
 // This begins the interesting code:
+
+// This obtains an array of words from a dictionary.
+let words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+console.log(words.length);
+// This generates a random word.
+function guessWord(){
+  return words[Math.floor(Math.random() * (235886 + 1)) + 0];
+}
+let word = guessWord();
+console.log("Word: " + word);
 
 
 // This controls the localhost page.
@@ -48,7 +69,7 @@ app.get("/", function (req, res) {
 });
 
 let guessCount = 0;
-let word = 'example';
+
 
 // This is called by submitting the form on the index page
 // This is called by submitting the form on the gameplay page.
@@ -56,7 +77,6 @@ app.post("/guess_game", function (req, res) {
   // word = 'term3';
   let letter = req.body.guess;
   if(guessCount !== 0){
-
   if(letter.length > 1 || letter.length === 0){
     console.log("Guess not one character");
     res.render('gameplay', {blanks: word, count: guessCount});
