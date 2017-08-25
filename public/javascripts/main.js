dcd21e0ffe6715a01ca19c3cbaef4ee5dd99bbf8
Kevin Harrison <kharrison7@elon.edu>
2017-08-25 14:31:48 -0400

    This finds capital letters and consolelogs them in dev tools.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,21 @@
 // This is space for any JS
-let text1 = document.getElementById("wordGuess");
-let text3 = text1.innerHTML;
-console.log("Text: "+text3);
+let text1 = document.getElementById("wordGuess").innerHTML;
+// let text3 = text1.innerHTML;
+console.log("Text: "+text1);
 
   // for(i=0; i < text.length; i++) {
   //     if(text[i].charCodeAt(text[i]) >= 65 && text[i].charCodeAt(text[i]) <= 90)
   //     wrapNode(text[i], 'span');
   // }
+
+
+  function wrapCaps() {
+    console.log("function called")
+    for(i=0; i < text1.length; i++) {
+      // The numbers are the unicode for capital letters.
+        if(text1[i].charCodeAt(text1[i]) >= 65 && text1[i].charCodeAt(text1[i]) <= 90){
+          console.log("Capital: "+text1[i]);
+        }
+      }
+   }
+wrapCaps();
